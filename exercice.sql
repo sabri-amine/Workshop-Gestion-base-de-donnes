5d768a41e64dc44a260fc84b7ebd3e60c4026b63
Sabri Amine <124732319+sabri-amine@users.noreply.github.com>
2024-12-06 11:50:51 +0100

    create table livres

--- exercice.sql
+++ exercice.sql
@@ -1,3 +1,11 @@
 -- 1)
 CREATE DATABASE Librairie
 use Librairie
+
+CREATE TABLE livre (
+    id	INT	PRIMARY KEY,
+    titre VARCHAR(255) NOT NULL,
+    auteur	VARCHAR(255) NOT NULL,
+    prix DECIMAL(5,2)NOT NULL,
+    stock INT NOT NULL
+    )
